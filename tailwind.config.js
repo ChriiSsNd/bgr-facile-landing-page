4b524b539abf1178cc6e75e98ac30e7ad52f84c8
Chris.Nd <chrisndouassi@gmail.com>
2022-02-23 23:35:33 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    "./public/**/*.html"
+  ],
+  theme: {
+    extend: {
+      // backgroundImage: (theme) => ({
+      //   'hero-pattern': "url('public/images/bg-hero-desktop.svg')",
+      // })
+    },
+  },
+  plugins: [],
+}
